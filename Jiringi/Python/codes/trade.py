05ca2584cba715e63e42c0eb9815aa3bbb6c8348
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-07-25 01:06:37 +0430

    Finished data reading

--- Jiringi/Python/codes/trade.py
+++ Jiringi/Python/codes/trade.py
@@ -1,36 +1,30 @@
-from asyncio import tasks
-import asyncio
 import time
-import _thread as thread
 import codes.handlers as hd
+from threading import Thread
 
 class TradeReader:
 
     x_training = list()
     y_training = list()
     file_index = 0
-    VERBOSE = 0
-
-    loop = None
-    saving_tasks = set()
+    verbose = 0
+    saving_tasks = list()
 
     def __init__(self, input_size, output_size, batch_size, verbose = 2):
         self.INPUT_SIZE = input_size
         self.BUFFER_SIZE = input_size + output_size
         self.BATCH_SIZE = batch_size
-        self.VERBOSE = verbose
+        self.verbose = verbose
 
     def ReadData(self):
-        self.loop = asyncio.new_event_loop()
         hd.SqlQueryExecute('trade-selection', (self.BUFFER_SIZE, ), self.__data_handler)
-        for t in self.saving_tasks:
-            self.loop.run_until_complete(t)
+        for task in self.saving_tasks: task.join()
 
     def __data_handler(self, cursor):
         instrument = None
         buffer = list()
         
-        if self.VERBOSE >= 1:
+        if self.verbose >= 1:
             print("Reading data: ...", end='')
 
         dur = time.time()
@@ -44,7 +38,7 @@ class TradeReader:
                     self.__save_and_reset()
 
             buffer.append(row[-1])
-            if self.VERBOSE >= 2:
+            if self.verbose >= 2:
                 print("\rReading data: {}".format(row[0], self.file_index), end='')
 
             if len(buffer) < self.BUFFER_SIZE: continue
@@ -56,14 +50,14 @@ class TradeReader:
 
         self.__save_and_reset()
 
-        if self.VERBOSE >= 1:
+        if self.verbose >= 1:
             print("\nReading finished in {0:.2f} sec and {1} files".format(
                 time.time() - dur, self.file_index))
     
     def __save_and_reset(self):
         self.file_index += 1
 
-        if self.VERBOSE >= 4:
+        if self.verbose >= 4:
             x_length = len(self.x_training)
             x_depth = len(self.x_training[0])
             y_length = len(self.y_training)
@@ -75,17 +69,18 @@ class TradeReader:
             { "name": "trade-x-{}".format(self.file_index), "data": self.x_training },
             { "name": "trade-y-{}".format(self.file_index), "data": self.y_training }
         ]
-        saving_task = tasks.run_coroutine_threadsafe(self.__convert_and_save(args), self.loop)
-        self.saving_tasks.add(saving_task)
+        saving_task = Thread(target=self.__convert_and_save, args=(args, ))
+        self.saving_tasks.append(saving_task)
+        saving_task.start()
 
         self.x_training = list()
         self.y_training = list()
         
-    async def __convert_and_save(self, files):
+    def __convert_and_save(self, files):
         for file in files:
             file["shape"] = hd.SaveFile(file["name"], file["data"])
         
-        if self.VERBOSE >= 3:
+        if self.verbose >= 3:
             message = ""
             for file in files:
                 message += ", {}:{}".format(file["name"], file["shape"])
