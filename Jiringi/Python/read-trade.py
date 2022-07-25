5317f4e3112d3109f71fac128cbca5a17e6b2e91
hamedna <hamedna@systemgroup.net>
2022-07-25 18:05:11 +0430

    cleaning the code

--- Jiringi/Python/read-trade.py
+++ Jiringi/Python/read-trade.py
@@ -2,9 +2,3 @@ import codes.trade as trade
 
 loader = trade.TradeReader(300, 10, 280000, verbose=4)
 loader.ReadData(ignore_existing=True)
-
-# data = list()
-# data.append([10, 20])
-# data.append([10, 30])
-# print("data=", data)
-# print("data=", np.array(data).shape)
