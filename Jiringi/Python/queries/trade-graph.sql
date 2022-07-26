48805d5fb2b678fc8df3b890d65a8f0cb884c68d
hamedna <hamedna@systemgroup.net>
2022-07-26 12:58:52 +0430

    Change graph query

--- Jiringi/Python/queries/trade-graph.sql
+++ Jiringi/Python/queries/trade-graph.sql
@@ -2,6 +2,14 @@ declare @InstrumentID int = 13,
         @Offset int = 0,
         @Count int = 1000
 
-select t.ClosePrice, t.DateTimeEn
+select t.DateTimeEn
+     , t.BuyerCount
+     , t.OpenPrice
+     , t.LowPrice
+     , t.HighPrice
+     , t.ClosePrice
+     , t.RecordType
+     , t.TradeCount
+     , isnull(t.Volume, 0) as Volume
 from Trade t where t.InstrumentID = @InstrumentID
 order by t.DateTimeEn offset @Offset rows fetch next @Count rows only
