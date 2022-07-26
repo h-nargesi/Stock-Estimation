d97f81953ed3d76ef23f14d846f2a2413d3a3f02
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-07-26 05:26:37 +0430

    improve messages

--- Jiringi/Python/codes/trade.py
+++ Jiringi/Python/codes/trade.py
@@ -53,7 +53,7 @@ class TradeReader:
 
             buffer.append(row[-1])
             if self.VERBOSE >= 2:
-                print("\rReading data: {}\t{}".format(row[0]), end='')
+                print("\rReading data: {}".format(row[0]), end='')
                 if self.message is not None:
                     print("\t{}".format(self.message), end='')
 
@@ -77,16 +77,17 @@ class TradeReader:
         self.file_index += 1
 
         if self.VERBOSE >= 4:
+            self.message = None
             x_length = len(self.x_training)
             x_depth = len(self.x_training[0])
             y_length = len(self.y_training)
             y_depth = len(self.y_training[0])
-            self.message = "saving file: {} with shape: x={}, y={}".format(
+            self.message = "saving files ({}): x={}, y={}".format(
                 self.file_index, (x_length, x_depth), (y_length, y_depth))
 
         args = [
-            { "name": "trade-x-{}".format(self.file_index), "data": self.x_training },
-            { "name": "trade-y-{}".format(self.file_index), "data": self.y_training }
+            { "name": "trade-x-{}".format(self.file_index), "data": self.x_training, "key": "x" },
+            { "name": "trade-y-{}".format(self.file_index), "data": self.y_training, "key": "y" }
         ]
         saving_task = Thread(target=self.__convert_and_save, args=(args, self.file_index, ))
         self.saving_tasks.append(saving_task)
@@ -100,11 +101,12 @@ class TradeReader:
             file["shape"] = hd.SaveFile(file["name"], file["data"])
         
         if self.VERBOSE >= 3:
-            message = ""
+            self.message = None
+            files_info = ""
             for file in files:
-                message += ", {}:{}".format(file["name"], file["shape"])
-            if len(message) > 0: message = message[2:]
-            self.message = "files ({}) was saved: {}".format(index, message)
+                files_info += ", {}={}".format(file["key"], file["shape"])
+            if len(files_info) > 0: files_info = files_info[2:]
+            self.message = "files ({}) have been saved: {}".format(index, files_info)
     
     def __wait_all_tasks_finished(self):
         for task in self.saving_tasks:
