bea58f39cfd232994cd37e7460ceb8664e2ea151
hamedna <hamedna@systemgroup.net>
2022-07-27 15:01:51 +0430

    solution_1_simple_bigstep

---
+++ Jiringi/Python/solution_1_simple_bigstep/query.sql
@@ -0,0 +1,43 @@
+declare @Minsize int = 310
+
+select ROW_NUMBER() OVER(order by t.InstrumentID, t.DateTimeEn) as Ranking
+    , t.InstrumentID
+    , RowCounts as Size
+    , CAST(CASE WHEN CloseIncreasing > 10 THEN 10 WHEN CloseIncreasing < -10 THEN -10 ELSE CloseIncreasing END AS FLOAT) as CloseIncreasing
+from (
+    select Trade.InstrumentID, RowCounts, DateTimeEn
+        , ROW_NUMBER() OVER(partition by Trade.InstrumentID order by DateTimeEn) as Ranking
+        , ClosePriceChange / ClosePrice as CloseIncreasing
+    from Trade
+	join (
+		select InstrumentID, RowCounts
+			from (
+			select top 80 percent dens.InstrumentID, RowCounts
+				 , ROW_NUMBER() over(order by Duration) +
+				   ROW_NUMBER() over(order by Density) * 10 as Score
+			from (
+				select InstrumentID, Duration, RowCounts
+					 , CASE Duration WHEN 0 THEN 0 ELSE CAST(RowCounts AS FLOAT) / CAST(Duration AS FLOAT) END AS Density
+				from (
+					select InstrumentID
+						 , ISNULL(DATEDIFF(DAY, StartDateTime, EndDateTime), 0) as Duration
+						 , RowCounts
+					from (
+						select InstrumentID
+							 , MIN(DateTimeEn) as StartDateTime
+							 , MAX(DateTimeEn) as EndDateTime
+							 , COUNT(1) as RowCounts
+						from Trade
+						group by InstrumentID
+						having COUNT(1) >= @Minsize
+
+					) ins
+				) dur
+			) dens
+			order by Score desc
+		) scr
+	) ValidInstruments
+	on ValidInstruments.InstrumentID = Trade.InstrumentID
+) t
+where t.Ranking > 1
+order by InstrumentID, DateTimeEn
