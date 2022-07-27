d000f67fbfe6060f5d62f30961d03a52bba70eaa
hamedna <hamedna@systemgroup.net>
2022-07-27 17:59:18 +0430

    Change model for solution-1-simple-bigstep

--- Jiringi/Python/solution_1_simple_bigstep/model.py
+++ Jiringi/Python/solution_1_simple_bigstep/model.py
@@ -4,26 +4,28 @@ def GetName(): return "solution_1_simple_bigstep"
 
 def GetModel(input_shape, output_size):
     print("Model: {} -> {}".format(input_shape, output_size))
-    DROPOUPT_VALUE = 0.3
+    DROPOUPT_VALUE = 0.66
 
     model = keras.models.Sequential(name=GetName())
     model.add(keras.Input(shape=input_shape))
 
-    model.add(keras.layers.Conv2D(20, (50, 1), strides=(5, 1), activation='gelu'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(10, 1), strides=(5, 1), padding="valid"))
+    features = input_shape[2] * 20
+    model.add(keras.layers.Conv2D(features, (30, 1), strides=(2, 1), activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
-    model.add(keras.layers.Conv2D(40, (25, 1), strides=(5, 1), activation='gelu'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(10, 1), strides=(5, 1), padding="valid"))
+
+    features += input_shape[2] * 10
+    model.add(keras.layers.Conv2D(features, (15, 1), strides=(2, 1), activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
-    model.add(keras.layers.Conv2D(60, (10, 1), strides=(1, 1), activation='gelu'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(5, 1), strides=(1, 1), padding="valid"))
+
+    features += input_shape[2] * 5
+    model.add(keras.layers.Conv2D(features, (10, 1), strides=(3, 1), activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
 
     model.add(keras.layers.Flatten())
 
-    model.add(keras.layers.Dense(500, activation="gelu"))
+    model.add(keras.layers.Dense(100, activation="gelu"))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
-    model.add(keras.layers.Dense(250, activation="gelu"))
+    model.add(keras.layers.Dense(50, activation="gelu"))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
     model.add(keras.layers.Dense(output_size, activation="linear"))
 
