b31ee37a7328af8973594a55d7f326f93588adeb
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-07-28 10:11:04 +0430

    Check dollar

--- Jiringi/Python/queries/trade-graph.sql
+++ Jiringi/Python/queries/trade-graph.sql
@@ -3,7 +3,7 @@ declare @InstrumentIDs varchar(max) = '13',
         @Offset int = 0,
         @Count int = 400
 
-select InstrumentID
+select Instrument
 	 , MAX(t.DateTimeEn) as DateTimeEn
 	 , AVG(t.BuyerCount) as BuyerCount
 	 , AVG(t.OpenPrice) as OpenPrice
@@ -13,7 +13,7 @@ select InstrumentID
 	 , AVG(t.TradeCount) as TradeCount
 	 , AVG(t.Volume) as Volume
 from (
-	select InstrumentID
+	select i.NameEn as Instrument
 		 , t.DateTimeEn
 		 , ISNULL(BuyerCount, 0) AS BuyerCount
 		 , ISNULL(OpenPrice, 0) AS OpenPrice
@@ -24,7 +24,8 @@ from (
 		 , ISNULL(Volume, 0) as Volume
 		 , ROW_NUMBER() over(partition by InstrumentID order by DateTimeEn) / @Zoom as GroupingValue
 	from Trade t
+	join Instrument i on t.InstrumentID = i.ID
 	where t.InstrumentID in (select CAST(VALUE AS INT) AS VALUE from STRING_SPLIT(@InstrumentIDs, ','))
 ) t
 where GroupingValue between @Offset and @Offset + @Count
-group by InstrumentID, GroupingValue
+group by Instrument, GroupingValue
