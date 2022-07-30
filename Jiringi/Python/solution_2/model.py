4aef53daa25172b7a6432b3920b263c76af959da
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-07-30 22:44:05 +0430

    Improve solution-2

--- Jiringi/Python/solution_2/model.py
+++ Jiringi/Python/solution_2/model.py
@@ -10,24 +10,24 @@ def GetModel(input_shape, output_size):
     model.add(keras.Input(shape=input_shape))
 
     features = input_shape[2] * 20
-    model.add(keras.layers.Conv2D(features, (30, 1), strides=(2, 1), activation='gelu'))
+    model.add(keras.layers.Conv2D(features, (30, 1), strides=(2, 1), kernel_initializer='normal', activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
 
     features += input_shape[2] * 10
-    model.add(keras.layers.Conv2D(features, (15, 1), strides=(2, 1), activation='gelu'))
+    model.add(keras.layers.Conv2D(features, (15, 1), strides=(2, 1), kernel_initializer='normal', activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
 
     features += input_shape[2] * 5
-    model.add(keras.layers.Conv2D(features, (10, 1), strides=(3, 1), activation='gelu'))
+    model.add(keras.layers.Conv2D(features, (10, 1), strides=(3, 1), kernel_initializer='normal', activation='gelu'))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
 
     model.add(keras.layers.Flatten())
 
-    model.add(keras.layers.Dense(100, activation="gelu"))
+    model.add(keras.layers.Dense(100, kernel_initializer='normal', activation="gelu"))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
-    model.add(keras.layers.Dense(50, activation="gelu"))
+    model.add(keras.layers.Dense(50, kernel_initializer='normal', activation="gelu"))
     model.add(keras.layers.Dropout(DROPOUPT_VALUE))
-    model.add(keras.layers.Dense(output_size, activation="linear"))
+    model.add(keras.layers.Dense(output_size, kernel_initializer='normal', activation="linear"))
 
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
