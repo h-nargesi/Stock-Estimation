0a3cac4f04f7b5ffbaafd0a67e16f556aca9220c
hamedna <hamedna@systemgroup.net>
2022-08-01 22:50:14 +0430

    Solution-1-simple-10

---
+++ Jiringi/Python/solution_1_simple_10/model.py
@@ -0,0 +1,41 @@
+import keras
+import codes.handlers as hd
+
+def GetName(): return "solution_1_simple_10"
+
+def GetModel(input_shape, output_size):
+    print("Model: {} -> {}".format(input_shape, output_size))
+    DROPOUPT_VALUE_CNN = 0.2
+    DROPOUPT_VALUE_FC = 0.7
+
+    model = keras.models.Sequential(name=GetName())
+    model.add(keras.Input(shape=input_shape))
+
+    features = input_shape[2] * 20
+    model.add(keras.layers.Conv2D(features, (30, 1), strides=(2, 1), activation='gelu'))
+    model.add(keras.layers.Dropout(DROPOUPT_VALUE_CNN))
+
+    features += input_shape[2] * 10
+    model.add(keras.layers.Conv2D(features, (15, 1), strides=(3, 1), activation='gelu'))
+    model.add(keras.layers.Dropout(DROPOUPT_VALUE_CNN))
+
+    features += input_shape[2] * 10
+    model.add(keras.layers.Conv2D(features, (10, 1), strides=(4, 1), activation='gelu'))
+    model.add(keras.layers.Dropout(DROPOUPT_VALUE_CNN))
+
+    model.add(keras.layers.Flatten())
+
+    model.add(keras.layers.Dense(100, activation="gelu"))
+    model.add(keras.layers.Dropout(DROPOUPT_VALUE_FC))
+    model.add(keras.layers.Dense(50, activation="gelu"))
+    model.add(keras.layers.Dropout(DROPOUPT_VALUE_FC))
+    model.add(keras.layers.Dense(output_size, activation="linear"))
+
+    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+    model.summary()
+
+    return model
+
+def PrintResult(score):
+    hd.PrintPerentage('Result:', score, ".3f", "%", 10)
---
+++ Jiringi/Python/solution_1_simple_10/query.sql
@@ -0,0 +1,17 @@
+declare @MinSize int = 310
+
+select ROW_NUMBER() OVER (order by InstrumentID, DateTimeEn) as Ranking
+	, TradeNo - 1 as TradeNo
+	, InstrumentID
+	, RowCounts - 1 as RowCounts
+    , 10 * CAST(CASE WHEN CloseIncreasing > 0.1 THEN 0.1 WHEN CloseIncreasing < -0.1 THEN -0.1 ELSE CloseIncreasing END AS FLOAT) as CloseIncreasing
+from (
+    select Trade.InstrumentID, RowCounts, DateTimeEn
+        , ROW_NUMBER() OVER(partition by Trade.InstrumentID order by DateTimeEn) as TradeNo
+        , ISNULL(ClosePriceChange / ClosePrice, 0) as CloseIncreasing
+    from Trade
+	join ActiveInstuments(@MinSize) ValidInstruments
+	on ValidInstruments.InstrumentID = Trade.InstrumentID
+) t
+where t.TradeNo > 1
+order by InstrumentID, DateTimeEn
