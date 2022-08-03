ae785be60ca3959edecb25f58356573815da0d4e
hamedna <hamedna@systemgroup.net>
2022-08-03 22:41:59 +0430

    solution 1

--- Jiringi/Python/solution_1/model.py
+++ Jiringi/Python/solution_1/model.py
@@ -17,19 +17,19 @@ class Modelling(ModelHandlers):
         model.add(keras.Input(shape=input_shape))
 
         features = input_shape[2] * 20
-        model.add(keras.layers.Conv2D(features, (40, 1), strides=(5, 1), activation='relu'))
+        model.add(keras.layers.Conv2D(features, (40, 1), strides=(5, 1), kernel_initializer='normal', activation='relu'))
         model.add(keras.layers.Dropout(DROPOUPT_VALUE_CNN))
         
         features += input_shape[2] * 20
-        model.add(keras.layers.Conv2D(features, (10, 1), strides=(2, 1), activation='relu'))
+        model.add(keras.layers.Conv2D(features, (10, 1), strides=(2, 1), kernel_initializer='normal', activation='relu'))
         model.add(keras.layers.Dropout(DROPOUPT_VALUE_CNN))
 
         model.add(keras.layers.Flatten())
 
-        model.add(keras.layers.Dense(240, activation="relu"))
+        model.add(keras.layers.Dense(240, kernel_initializer='normal', activation="relu"))
         model.add(keras.layers.Dropout(DROPOUPT_VALUE_FC))
 
-        model.add(keras.layers.Dense(240, activation="relu"))
+        model.add(keras.layers.Dense(240, kernel_initializer='normal', activation="relu"))
         model.add(keras.layers.Dropout(DROPOUPT_VALUE_FC))
 
         model.add(keras.layers.Dense(output_size, activation="linear"))
