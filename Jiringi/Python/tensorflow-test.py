b8109057b25f3f633e00dcac9a00e5861d92c8b0
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2022-08-04 15:18:32 +0430

    Saving method

--- Jiringi/Python/tensorflow-test.py
+++ Jiringi/Python/tensorflow-test.py
@@ -1,3 +1,4 @@
+from keras.models import load_model
 from codes.handlers import Handlers
 from solution_1_simple_10.model import Modelling
 
@@ -10,23 +11,20 @@ print("\n# Data\n")
 
 x_testing, y_testing = hd.LoadTestData(1)
 
-# Modeling
-print("\n# Modeling\n")
-modeling = Modelling(hd.LoadOptions()["Factor"])
-model = Modelling.GetModel(x_testing.shape[1:], y_testing.shape[-1])
+# Loading Models
 
 models = hd.ModelList()
 for check_point_path in models:
     print('\n[{}]'.format(check_point_path))
-    model.load_weights("{}/model/{}".format(Modelling.NAME, check_point_path))
+    model = load_model("{}/model/{}".format(Modelling.NAME, check_point_path))
 
     # Evaluation
     print("\n# Evaluation")
     score = model.evaluate(x_testing, y_testing, verbose=0)
-    modeling.PrintResult(score)
+    model.PrintResult(score)
 
     # Prediction
     print("\n# Prediction")
     predicted = model.predict(x_testing, verbose=0)
-    average = modeling.Prediction(predicted, y_testing)
-    modeling.PrintResult(average)
+    average = model.Prediction(predicted, y_testing)
+    model.PrintResult(average)
