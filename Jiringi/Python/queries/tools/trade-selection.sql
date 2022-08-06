f8ed39919bdccd8ef26a2167253eb00b0e3cb51b
hamedna <hamedna@systemgroup.net>
2022-08-06 16:50:26 +0430

    Query for real value just in close-price

--- Jiringi/Python/queries/tools/trade-selection.sql
+++ Jiringi/Python/queries/tools/trade-selection.sql
@@ -1,36 +1,60 @@
-declare @MinSize int = 310
+declare @MinSize int = 310,
+        @Factor int = 100;
+
+with UniqueTrade as (
+	select InstrumentID, DateTimeEn, ClosePriceChange
+		, ClosePrice, LowPrice, HighPrice, OpenPrice
+	from (
+		select InstrumentID, DateTimeEn, ClosePriceChange
+			, ClosePrice, LowPrice, HighPrice, OpenPrice
+			, ROW_NUMBER() OVER (partition by InstrumentID, DateTimeEn order by ID desc) as ranking
+		from Trade
+	) t
+	where ranking = 1
+)
 
 select ROW_NUMBER() OVER (order by InstrumentID, DateTimeEn) as Ranking
 	, TradeNo - 1 as TradeNo
 	, InstrumentID
 	, RowCounts - 1 as RowCounts
-	, CAST(CloseIncreasing AS FLOAT) as CloseIncreasing
-	, CAST(HighIncreasing AS FLOAT) as HighIncreasing
-	, CAST(LowIncreasing AS FLOAT) as LowIncreasing
-	, CAST(OpenIncreasing AS FLOAT) as OpenIncreasing
+	, @Factor * CAST(CloseIncreasing AS FLOAT) as CloseIncreasing
+	, @Factor * CAST(HighIncreasing AS FLOAT) as HighIncreasing
+	, @Factor * CAST(LowIncreasing AS FLOAT) as LowIncreasing
+	, @Factor * CAST(OpenIncreasing AS FLOAT) as OpenIncreasing
 	, DATEDIFF(DAY, '1921-03-21', DateTimeEn) as DurationDays
 	, d.Code2 as IndustryCode
 from (
 	select InstrumentID, RowCounts, DateTimeEn, TradeNo
-		, ISNULL(ClosePriceChange, CalcClosePriceChange) / ClosePrice as CloseIncreasing
-		, (LowPrice - LowPriceChange) / LowPrice as LowIncreasing
-		, (HighPrice - HighPriceChange) / HighPrice as HighIncreasing
-		, (OpenPrice - OpenPriceChange) / OpenPrice as OpenIncreasing
+		, (ClosePrice - ClosePricePrv) / ClosePrice as CloseIncreasing
+		, (LowPrice - LowPricePrv) / LowPrice as LowIncreasing
+		, (HighPrice - HighPricePrv) / HighPrice as HighIncreasing
+		, (OpenPrice - OpenPricePrv) / OpenPrice as OpenIncreasing
 	from (
 		select Trade.InstrumentID, RowCounts, DateTimeEn
 			, ROW_NUMBER() OVER(partition by Trade.InstrumentID order by DateTimeEn) as TradeNo
-			, ClosePrice
-			, LowPrice
-			, HighPrice
-			, OpenPrice
-			, ClosePriceChange
-			, LAG(ClosePrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as CalcClosePriceChange
-			, LAG(LowPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as LowPriceChange
-			, LAG(HighPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as HighPriceChange
-			, LAG(OpenPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as OpenPriceChange
-		from Trade
+			, ClosePrice / DollarClosePrice as ClosePrice
+			, LowPrice / DollarLowPrice as LowPrice
+			, HighPrice / DollarHighPrice as HighPrice
+			, OpenPrice / DollarOpenPrice as OpenPrice
+			, LAG(ClosePrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as ClosePricePrv
+			, LAG(LowPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as LowPricePrv
+			, LAG(HighPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as HighPricePrv
+			, LAG(OpenPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as OpenPricePrv
+		from UniqueTrade Trade
 		join ActiveInstuments(@MinSize) ValidInstruments
 		on ValidInstruments.InstrumentID = Trade.InstrumentID
+		left join (
+			select DateTimeEn as StartDateTimeEn
+				, DATEADD(DAY, -1, LEAD(DateTimeEn) OVER(order by DateTimeEn)) as EndDateTimeEn
+				, ClosePrice as DollarClosePrice
+				, LowPrice as DollarLowPrice
+				, HighPrice as DollarHighPrice
+				, OpenPrice as DollarOpenPrice
+				, LAG(ClosePrice) OVER (order by DateTimeEn) as DollarClosePricePrv
+			from UniqueTrade
+			where InstrumentID = 17321
+		) Dollar
+		on Trade.DateTimeEn between Dollar.StartDateTimeEn and ISNULL(Dollar.EndDateTimeEn, Trade.DateTimeEn)
 	) td
 ) t
 join Instrument i on i.ID = t.InstrumentID
