82125682662803e7ffcaa73d9b1da66aea92fc61
ryan <ryan.nargesi.ai@gmail.com>
2023-07-17 12:19:38 +0330

    noise

--- Jiringi/queries/noises/close-price-change.sql
+++ Jiringi/queries/noises/close-price-change.sql
@@ -11,3 +11,12 @@ from (
 	on v.InstrumentID = t.InstrumentID
 ) t
 where ClosePriceChange is null or CalcClosePriceChange != ClosePriceChange
+
+select *
+from (
+	select t.DateTimeEn, t.ClosePrice, t.ClosePriceChange, p.DateTimeEn as PrvDateTimeEn, p.ClosePrice as PrvClosePrice
+		, t.ClosePrice - p.ClosePrice as CalcClosePriceChange
+	from Trade t
+	join Trade p on t.PreviousID = p.ID
+) t
+where t.ClosePriceChange is null or t.CalcClosePriceChange != t.ClosePriceChange
