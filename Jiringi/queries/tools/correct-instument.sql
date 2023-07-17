6cf8a3cc31adf99997863f04c94b6655def1a867
ryan <ryan.nargesi.ai@gmail.com>
2023-07-17 12:19:19 +0330

    Instrument selection

--- Jiringi/queries/tools/correct-instument.sql
+++ Jiringi/queries/tools/correct-instument.sql
@@ -9,10 +9,10 @@ go
 drop function if exists ActiveInstuments
 go
 
-create function ActiveInstuments(@MinSize int)
+create function ActiveInstuments(@MinSize int, @Count int)
 returns table as
 return
-    select top 80 percent InstrumentID, RowCounts, Duration, Density
+    select InstrumentID, RowCounts, Duration, Density
         , ROW_NUMBER() over(order by Duration) + ROW_NUMBER() over(order by Density) * 10 as Score
     from (
         select InstrumentID, RowCounts, Duration
@@ -20,7 +20,7 @@ return
         from (
             select i.ID as InstrumentID, RowCounts
                 , ISNULL(DATEDIFF(DAY, StartDateTime, EndDateTime), 0) as Duration
-            from Instrument i left join Company c on i.CompanyID = c.ID
+            from Instrument i
             join (
                 select InstrumentID
                     , MIN(DateTimeEn) as StartDateTime
@@ -36,17 +36,20 @@ return
                 group by InstrumentID
                 having COUNT(1) >= @MinSize
             ) t on t.InstrumentID = i.ID
-            where i.TypeID in (1 /*Share*/, 19 /*Currency*/)
-			  and (c.ID is null or c.TypeID = 1 and c.StateID = 1)
+			left join Company c on i.CompanyID = c.ID
+			where i.TypeID = 1 /*Share*/ and c.TypeID = 1 and c.StateID = 1
+			   or i.TypeID = 19 /*Currency*/
+			--where i.TypeID in (1 /*Share*/, 19 /*Currency*/)
+			--  and (c.ID is null or c.TypeID = 1 and c.StateID = 1)
         ) dur
     ) dens
-    order by Score desc
+    order by Score desc offset 0 rows fetch next @Count rows only
 GO
 
 merge into ValidInstruments dst
-using (select InstrumentID from ActiveInstuments(320)) src
+using (select InstrumentID from ActiveInstuments(320, 10)) src
 on (dst.InstrumentID = src.InstrumentID)
 when not matched by target then insert (InstrumentID) values (src.InstrumentID)
 when not matched by source then delete;
 
-select * from ValidInstruments
+select * from ActiveInstuments(320, 10) order by Score desc
