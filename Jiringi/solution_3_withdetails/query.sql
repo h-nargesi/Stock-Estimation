62e8460988ae45ba58bc558cdc91b6d02e4b9aa7
Ryan Nargesi <ryan.nargesi.ai@gmail.com>
2023-07-20 00:49:00 +0330

    query currection

--- Jiringi/solution_3_withdetails/query.sql
+++ Jiringi/solution_3_withdetails/query.sql
@@ -1,13 +1,13 @@
 declare @MinSize int = 310,
         @Count int = 10,
-        @Factor int = 100;
+        @Factor int = 20;
 
 with UniqueTrade as (
-	select InstrumentID, DateTimeEn, ClosePriceChange
-		, ClosePrice, LowPrice, HighPrice, OpenPrice
+	select InstrumentID, DateTimeEn, ClosePrice
+		--, LowPrice, HighPrice, OpenPrice--, ClosePriceChange
 	from (
-		select InstrumentID, DateTimeEn, ClosePriceChange
-			, ClosePrice, LowPrice, HighPrice, OpenPrice
+		select InstrumentID, DateTimeEn, ClosePrice
+			--, LowPrice, HighPrice, OpenPrice--, ClosePriceChange
 			, ROW_NUMBER() OVER (partition by InstrumentID, DateTimeEn order by ID desc) as ranking
 		from Trade
 	) t
@@ -19,26 +19,26 @@ select ROW_NUMBER() OVER (order by InstrumentID, DateTimeEn) as Ranking
 	, InstrumentID
 	, RowCounts - 1 as RowCounts
 	, @Factor * CAST(CloseIncreasing AS FLOAT) as CloseIncreasing
-	, @Factor * CAST(HighIncreasing AS FLOAT) as HighIncreasing
-	, @Factor * CAST(LowIncreasing AS FLOAT) as LowIncreasing
-	, @Factor * CAST(OpenIncreasing AS FLOAT) as OpenIncreasing
+	-- , @Factor * CAST(HighIncreasing AS FLOAT) as HighIncreasing
+	-- , @Factor * CAST(LowIncreasing AS FLOAT) as LowIncreasing
+	-- , @Factor * CAST(OpenIncreasing AS FLOAT) as OpenIncreasing
 from (
 	select InstrumentID, RowCounts, DateTimeEn, TradeNo
 		, (ClosePrice - ClosePricePrv) / ClosePrice as CloseIncreasing
-		, (LowPrice - LowPricePrv) / LowPrice as LowIncreasing
-		, (HighPrice - HighPricePrv) / HighPrice as HighIncreasing
-		, (OpenPrice - OpenPricePrv) / OpenPrice as OpenIncreasing
+		-- , (LowPrice - LowPricePrv) / LowPrice as LowIncreasing
+		-- , (HighPrice - HighPricePrv) / HighPrice as HighIncreasing
+		-- , (OpenPrice - OpenPricePrv) / OpenPrice as OpenIncreasing
 	from (
 		select Trade.InstrumentID, RowCounts, DateTimeEn
 			, ROW_NUMBER() OVER(partition by Trade.InstrumentID order by DateTimeEn) as TradeNo
 			, ClosePrice as ClosePrice
-			, LowPrice as LowPrice
-			, HighPrice as HighPrice
-			, OpenPrice as OpenPrice
+			-- , LowPrice as LowPrice
+			-- , HighPrice as HighPrice
+			-- , OpenPrice as OpenPrice
 			, LAG(ClosePrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as ClosePricePrv
-			, LAG(LowPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as LowPricePrv
-			, LAG(HighPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as HighPricePrv
-			, LAG(OpenPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as OpenPricePrv
+			-- , LAG(LowPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as LowPricePrv
+			-- , LAG(HighPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as HighPricePrv
+			-- , LAG(OpenPrice) OVER(partition by Trade.InstrumentID order by DateTimeEn) as OpenPricePrv
 		from UniqueTrade Trade
 		join ActiveInstuments(@MinSize, @Count) ValidInstruments on ValidInstruments.InstrumentID = Trade.InstrumentID
 	) t
